6d57ef15e174042b58aa8453b441be2662737e83
Alexey Gilman <Alexey.Gilman@doh.wa.gov>
2021-09-24 17:54:39 -0700

    functionalize export of LIMS data

--- viral_lims_export.py
+++ viral_lims_export.py
@@ -2,11 +2,17 @@ import pyodbc
 import pandas as pd
 from lims_login import credentials
 
-cnxn = pyodbc.connect(credentials) # credentials = 'DSN=LIMS_DATA;UID=xxxxxxx;PWD=xxxxxxx'
-sql_query = pd.read_sql_query('SELECT * FROM [vz_Epi_ELS_SARS-CoV-2 ddPCR]',cnxn)
+def export_df_from_LIMS():
+    """
+    Create ODBC connection with SQL LIMS database. Return all waster water LIMS data as dataframe. 
+    return: Dataframe
+    
+    ######## Alternate way to pull SQL data object ##########
+    #cursor = cnxn.cursor()
+    #cursor.execute('SELECT * FROM [vz_Epi_ELS_SARS-CoV-2 ddPCR]')
+    """
+    
+    cnxn = pyodbc.connect(credentials) # credentials = 'DSN=LIMS_DATA;UID=xxxxxxx;PWD=xxxxxxx'
+    sql_query = pd.read_sql_query('SELECT * FROM [vz_Epi_ELS_SARS-CoV-2 ddPCR]',cnxn)
 
-######## Alternate way to pull SQL data object ##########
-#cursor = cnxn.cursor()
-#cursor.execute('SELECT * FROM [vz_Epi_ELS_SARS-CoV-2 ddPCR]')
-
-print(sql_query.info())
+    return sql_query
