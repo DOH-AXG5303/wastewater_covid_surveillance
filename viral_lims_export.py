fe98d0abf21a110e59353f1785cc14c38ba98cf6
Alexey Gilman <Alexey.Gilman@doh.wa.gov>
2021-09-28 19:54:14 -0700

    updated API export function, continue development and API experimentation

--- viral_lims_export.py
+++ viral_lims_export.py
@@ -1,6 +1,11 @@
 import pyodbc
 import pandas as pd
+import requests
+import io
 from lims_login import credentials
+from lims_login import redcap_tokens_prod
+from lims_login import redcap_api_url
+
 
 def export_df_from_LIMS():
     """
@@ -16,3 +21,20 @@ def export_df_from_LIMS():
     sql_query = pd.read_sql_query('SELECT * FROM [vz_Epi_ELS_SARS-CoV-2 ddPCR]',cnxn)
 
     return sql_query
+
+def redcap_API_export(url,token):
+    """
+    all data API request
+    """
+
+    fields = {
+        'token': token,
+        'content': 'record',
+        'format': 'csv',
+        'type': 'flat'}
+
+    r = requests.post(url, data=fields)
+    
+    df = pd.read_csv(io.StringIO(r.content.decode("utf-8")))
+
+    return df
