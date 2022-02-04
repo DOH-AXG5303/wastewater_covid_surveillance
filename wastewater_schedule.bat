1768536c80b2d53787e3cda13a868aa79e446c99
Gilman <Alexey.Gilman@doh.wa.gov>
2022-02-03 17:21:23 -0800

    adding .bat file for scheduling

---
+++ wastewater_schedule.bat
@@ -0,0 +1,4 @@
+call C:\Users\AXG5303\Miniconda3\Scripts\activate.bat
+call activate wastewater_env
+python C:\Users\AXG5303\AXG5303_repos\wastewater_covid_surveillance\viral_lims_export.py
+call conda deactivate
