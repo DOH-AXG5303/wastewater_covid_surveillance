dc13447d56e91e6a9dce296ad7b9fa35d26c8dde
Gilman <Alexey.Gilman@doh.wa.gov>
2022-06-16 10:36:29 -0700

    update batch file to v2 script

--- lims_to_redcap.bat
+++ lims_to_redcap.bat
@@ -1,4 +1,4 @@
 call C:\Users\AXG5303\Miniconda3\Scripts\activate.bat
-call activate wastewater_env
-python C:\Users\AXG5303\AXG5303_repos\wastewater_covid_surveillance\viral_lims_export.py
+call activate transform22
+python C:\Users\AXG5303\AXG5303_repos\wastewater_covid_surveillance\lims_export_v2.py
 call conda deactivate
